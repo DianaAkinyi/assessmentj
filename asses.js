9de93b454c5ff68582218193659327a62d9e86d3
DianaAkinyi <dianaakinyiowiti@gmail.com>
2023-04-11 09:05:06 +0300

    loss

---
+++ asses.js
@@ -0,0 +1,68 @@
+//Write a function that takes in a string and returns it when reversed
+// let food = 'eating'
+function foods(food){
+    return food.split("").reverse().join("")
+}
+console.log(foods("eating"))
+//Write a function that takes in the following array and consoles the target if it is found else
+// null
+// let num = [2,8,0,23,5,45,76]
+// Target = 23
+// function divide(num){
+//     if (num.length<1)
+//     return num
+//     let middle =Math.floor(num.length/2)
+//     let left =
+// }
+//Given years between 2000 and 2023, console all the leap years in the following
+//sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
+// even=[2000<=]
+// function evens(even){
+//     for(let i=0;i<length-1;i++){
+        
+//         return even[i]/=2
+//     }
+//      console.log(even[i])
+// }
+//  evens(even)
+
+
+
+//Write a function that takes in an array of numbers and returns an array that has all
+//elements multiplied by 4.
+let array = [12,87,45,75,23,64,73]
+ let multiplication=1
+let i=0
+function arrays(array){
+    for(i <array.length-1;i++;){
+    
+        return array[i]*=4
+    }
+    console.log(array[i])
+
+}
+arrays(array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Write a function that takes in an array of strings and returns an array with every element
+//turned into a number
+// let change = ["10","24","45","56","67"]
+ function  changes(change){
+    
+     return change.join()
+    
+    
+   
+ }
+console.log(changes(["10","24","45","56","67"]))
